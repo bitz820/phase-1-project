22299f09a89efccf4d0f0c305e55f6d7fc0e8663
Mark <mark.n.bacon.jr@gmail.com>
2022-03-28 18:55:21 -0400

    Mark and Andre created search/submit button and debugged scoping issues!

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
+// Bianca, Santiago
+
 let requestOptions = {
     method: 'GET',
     redirect: 'follow'
 };
+
+function selectForm (){
+    const form = document.querySelector("#search-movies")
+    form.addEventListener("submit", (e)=> {
+        e.preventDefault()
+        const movie = (e.target[0].value)
+        fetchSearch(movie)
+    })
+}
+
+function fetchSearch(movie) {
+  fetch(`https://imdb-api.com/en/API/SearchMovie/k_fkl6yn80/${movie}`, requestOptions)  
+    .then(response => response.json())
+    .then(data => {console.log(data.results)
+        data.results.forEach(result => {createMovieCard(result)})
+    })
+    .catch(error => console.log('error', error))
+}
 
 function createMovieCard (result){
     const resultsContainer = document.querySelector(".results-container")
@@ -12,6 +32,7 @@ function createMovieCard (result){
     image.src = result.image
     const watchBtn = document.createElement('button')
     watchBtn.innerText = "Add to Watch List!"
+    // watchBtn.addEventListener("click", )
     const seenBtn = document.createElement('button')
     seenBtn.innerText = "Add to Movies You've Seen!"
     movieCard.append(title, image, watchBtn, seenBtn)
@@ -19,17 +40,8 @@ function createMovieCard (result){
 }
 
 document.addEventListener("DOMContentLoaded", ()=>{
-    // fetchSearch()
+    selectForm()
 })
-
-function fetchSearch() {
-  fetch('https://imdb-api.com/en/API/SearchMovie/k_fkl6yn80/inception 2010', requestOptions)  
-    .then(response => response.json())
-    .then(data => {console.log(data.results)
-        data.results.forEach(result => {createMovieCard(result)})
-    })
-    .catch(error => console.log('error', error))
-}
 
 
 // {"searchType":"Movie","expression":"inception 2010","results":[{"id":"tt1375666","resultType":"Title","image":"https://imdb-api.com/images/original/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_Ratio0.6800_AL_.jpg","title":"Inception","description":"(2010)"},
