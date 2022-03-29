799eeca6908fdbb966b9a5f2af527bfecd300083
Mark <mark.n.bacon.jr@gmail.com>
2022-03-28 20:14:35 -0400

    added functionality to delete buttons, added basic color styling, started reivew button, but got stuck

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ function selectForm (){
     form.addEventListener("submit", (e)=> {
         e.preventDefault()
         const movie = (e.target[0].value)
+        e.target[0].value = ""
         fetchSearch(movie)
     })
 }
@@ -17,8 +18,9 @@ function selectForm (){
 function fetchSearch(movie) {
   fetch(`https://imdb-api.com/en/API/SearchMovie/k_fkl6yn80/${movie}`, requestOptions)  
     .then(response => response.json())
-    .then(data => {console.log(data.results)
-        data.results.forEach(result => {createMovieCard(result)})
+    .then(data => {data.results.forEach(
+        result => {createMovieCard(result)
+        })
     })
     .catch(error => console.log('error', error))
 }
@@ -26,18 +28,55 @@ function fetchSearch(movie) {
 function createMovieCard (result){
     const resultsContainer = document.querySelector(".results-container")
     const movieCard = document.createElement('card')
-    const title = document.createElement('h3')
+    const title = document.createElement('h2')
     title.innerText = result.title
     const image = document.createElement('img')
     image.src = result.image
+    image.className = "image"
     const watchBtn = document.createElement('button')
     watchBtn.innerText = "Add to Watch List!"
-    // watchBtn.addEventListener("click", )
+    watchBtn.addEventListener("click", () => {addToWatchList(result)})
     const seenBtn = document.createElement('button')
     seenBtn.innerText = "Add to Movies You've Seen!"
+    seenBtn.addEventListener("click", () => {addtoSeenList(result)})
     movieCard.append(title, image, watchBtn, seenBtn)
     resultsContainer.append(movieCard)
 }
+
+function handleRemove (){
+    removeBtn.previousSibling.remove()
+    removeBtn.remove()
+}
+
+function addToWatchList (result){
+    const watchList = document.querySelector(".watch")
+    const addWatch = document.createElement("li")
+    const removeBtn = document.createElement("button")
+    removeBtn.innerText = "Delete this movie!"
+    removeBtn.addEventListener("click", handleRemove)
+    addWatch.innerText = result.title
+    watchList.append(addWatch, removeBtn)
+}
+function addtoSeenList (result){
+    const seenList = document.querySelector(".seen")
+    const addSeen = document.createElement("li")
+    const removeBtn = document.createElement("button")
+    const reviewBtn = document.createElement("button")
+    // When button is clicked, add a form with an input and a submit button -- this value should append to the current li as a reivew
+    // reviewBtn.innerText = "Add a Reivew!"
+    // const review = document.createElement("input")
+    // reviewBtn.addEventListener("click", function (e){
+    //     console.log(e.target.input[1].value)
+    //     addSeen.append(review)
+    // })
+    // reviewBtn.append(review)
+    removeBtn.innerText = "Delete this movie!"
+    removeBtn.addEventListener("click", handleRemove)
+    addSeen.innerText = result.title
+    addSeen.append(reviewBtn)
+    seenList.append(addSeen, removeBtn)
+}
+
 
 document.addEventListener("DOMContentLoaded", ()=>{
     selectForm()
